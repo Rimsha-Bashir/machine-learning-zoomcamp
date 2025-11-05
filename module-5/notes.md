f6107aab41100070e236eec4720e833235f1f297
Rimsha Bashir Rahiman <82079501+Rimsha-Bashir@users.noreply.github.com>
2025-11-05 13:04:36 +0000

    update mod 5.1-5.2 notes

---
+++ module-5/notes.md
@@ -0,0 +1,19 @@
+## 5.1 Intro : Session Overview
+
+This week we learn how to deploy ML models as a web service.
+
+We'll take our churn prediction model, and 
+
+1. using flask, we'll put it inside a web service
+2. isolate the dependencies (create a special env for py dependencies using pip env) 
+3. another layer of for system dependencies, set up using Docker 
+4. deploy the entire module into cloud 
+
+![overview](images/5.1.png)
+
+
+## 5.2 Saving and Loading the Model
+
+[Check NB](./data-preparation.ipynb) - last few lines of code. 
+
+The model has been downloaded to module-4/eval_metrics folder
---
+++ module-5/predict.py
@@ -0,0 +1,57 @@
+
+
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+
+
+
+input_file = 'model_C=1.0.bin'
+
+with open(input_file, 'rb') as f_in: 
+    dv, model = pickle.load(f_in)
+
+
+
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+
+
+X = dv.transform([customer])
+
+
+
+y_pred = model.predict_proba(X)[0, 1]
+
+
+
+print('input:', customer)
+print('output:', y_pred)
+
