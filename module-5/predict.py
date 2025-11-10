3e82be12183986888d68371c13c34eacc84c1c44
Rimsha Bashir Rahiman <82079501+Rimsha-Bashir@users.noreply.github.com>
2025-11-10 20:24:35 +0000

    Update notes mod 5.4

--- module-5/predict.py
+++ module-5/predict.py
@@ -1,57 +1,32 @@
-
-
-import pandas as pd
-import numpy as np
 import pickle
 
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 
+model_file = 'model_C=1.0.bin'
 
-input_file = 'model_C=1.0.bin'
-
-with open(input_file, 'rb') as f_in: 
+with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
+app = Flask('churn')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
+
+    return jsonify(result)
 
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
-
-
-
-X = dv.transform([customer])
-
-
-
-y_pred = model.predict_proba(X)[0, 1]
-
-
-
-print('input:', customer)
-print('output:', y_pred)
-
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
